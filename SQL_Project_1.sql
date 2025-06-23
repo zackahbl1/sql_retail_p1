88636e9d868b2b63599015bbbb66eda6e1d7093f
Zephaniah Ackah-Blay <48698675+zackahbl1@users.noreply.github.com>
2025-06-23 14:31:29 -0400

    Update SQL_Project_1.sql

--- SQL_Project_1.sql
+++ SQL_Project_1.sql
@@ -5,19 +5,19 @@ CREATE DATABASE sql_project_p1;
 -- Create Table
 DROP TABLE IF EXISTS retail_sales;
 CREATE TABLE retail_sales
-			(
-				transactions_id	INT PRIMARY KEY,
-				sale_date DATE,	
-				sale_time TIME,
-				customer_id	INT,
-				gender VARCHAR(15),
-				age INT,
-				category VARCHAR(15),
-				quantity INT,
-				price_per_unit FLOAT,
-				cogs	FLOAT,
-				total_sale FLOAT
-			);
+(
+	transactions_id	INT PRIMARY KEY,
+	sale_date DATE,	
+	sale_time TIME,
+	customer_id	INT,
+	gender VARCHAR(15),
+	age INT,
+	category VARCHAR(15),
+	quantity INT,
+	price_per_unit FLOAT,
+	cogs	FLOAT,
+	total_sale FLOAT
+);
 
 SELECT *
 FROM retail_sales
